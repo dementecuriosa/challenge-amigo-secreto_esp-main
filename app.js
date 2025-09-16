347c2ba691446d03b8a26b5cd80c743d4b42bb24
dementecuiosa <alebookslat@gmail.com>
2025-09-15 19:23:19 -0500

    cambiamos el titulo

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//modificamos el titulo
+let titulo = document.querySelector(`h1`);
+titulo.innerHTML="Juego del amigo secreto"
+
 //crearemos un array
 let amigos = [ ];
